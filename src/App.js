025ce2a5536fb1799b061360d06bdda7630f8418
yarila682 <yarila682@yandex.ru>
2018-03-25 16:35:30 +0300

    More tests were added.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {RobotConrolAPI} from 'Robboscratch3_DeviceControlAPI';
+
+
+
 class App extends Component {
+
+constructor(){
+
+  super();
+
+  this.RCA =  new RobotConrolAPI();
+
+}
+
+connectorTest(){
+
+     this.RCA.searchRobotDevices();
+
+}
+
+stopSearchProcess(){
+
+if (this.RCA instanceof RobotConrolAPI){
+
+    this.RCA.stopSearchProcess();
+
+}
+
+}
+
+stopDataRecievingProcess(){
+
+
+  if (this.RCA instanceof RobotConrolAPI){
+
+      this.RCA.stopDataRecievingProcess();
+
+  }
+
+}
+
+
+
+
+setRobotPower(){
+
+
+  if (this.RCA instanceof RobotConrolAPI){
+
+    let leftMotorPower = document.getElementById("leftMotorPower").value;
+    let rightMotorPower = document.getElementById("rightMotorPower").value;
+
+    this.RCA.setRobotPower(leftMotorPower,rightMotorPower);
+
+  }
+
+}
+
   render() {
     return (
       <div className="App">
@@ -10,9 +67,31 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        
+
+        <div>
+
+              <button class="connector-start-stop" onClick={this.connectorTest.bind(this)}>Connector test!</button>
+              <button class="connector-start-stop" onClick={this.stopSearchProcess.bind(this)}>Connector stop!</button>
+              <button class="connector-start-stop" onClick={this.stopDataRecievingProcess.bind(this)}>Stop data recieve!</button>
+
+        </div>
+
+        <div>
+
+              <input id="leftMotorPower"  type="text"></input>
+              <input id="rightMotorPower" type="text"></input>
+
+        </div>
+
+        <div>
+
+            <button  onClick={this.setRobotPower.bind(this)}> Set motors power. </button>
+
+        </div>
+
+
       </div>
     );
   }
